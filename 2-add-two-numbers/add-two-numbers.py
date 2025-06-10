0635a0fa2ecb5507bd53cae211b7b4e60fa45c93
nsngary <nsngary@gmail.com>
2025-06-10 23:07:58 +0800

    Time: 2 ms (92.53%) | Memory: 12.6 MB (15.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -11,21 +11,25 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         dummy = ListNode()
-        current = dummy
+        head = dummy
         carry = 0
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0  # 如果 l1 結束就用 0
-            val2 = l2.val if l2 else 0
-
-            total = val1 + val2 + carry
-            carry = total // 10         # 決定進位
-            new_digit = total % 10      # 只保留個位數
-
-            current.next = ListNode(new_digit)
-            current = current.next
-
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-
-        return dummy.next        
+        while l1 and l2:
+            head.next = ListNode((l1.val + l2.val + carry) % 10)
+            carry = (l1.val + l2.val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+            head = head.next
+        while l1:
+            head.next = ListNode((l1.val + carry) % 10)
+            carry = (l1.val + carry) // 10
+            l1 = l1.next
+            head = head.next
+        while l2:
+            head.next = ListNode((l2.val + carry) % 10)
+            carry = (l2.val + carry) //10
+            l2 = l2.next
+            head = head.next
+        if carry == 1:
+            head.next = ListNode(1)
+        return dummy.next
