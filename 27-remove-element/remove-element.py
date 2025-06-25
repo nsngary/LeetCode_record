f4afa003f196bd252c3172a6e0fe91ea3701efa8
nsngary <nsngary@gmail.com>
2025-06-25 16:26:26 +0800

    Time: 5 ms (2.26%) | Memory: 12.5 MB (9.26%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,27 +5,11 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        j = len(nums)
+        k = 0
         for i in range(len(nums)):
-            if i<j:
-                left = nums[i]
-                right = nums[j - 1]
-                if right == val:
-                    j -= 1
-                right = nums[j - 1]
-                if left == val:
-                    uni = nums[i]
-                    nums[i] = right
-                    #right = uni
-                    nums[j - 1] = uni
-        sum = 0
-        for i in nums:
-            if i == val:
-                sum += 1
-
-        for i in range(sum):
-            nums.remove(val)
-        k = len(nums)
-        for i in range(sum):
-            nums.append(None)
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        print(k)
+        print(nums)
         return k
